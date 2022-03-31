ccf586f3e33d71b651f6efea8cddd410385ca10a
Arvin <joe52729@gmail.com>
2022-03-31 16:10:55 +0800

    [DB]會員migration建立

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,17 +8,19 @@ class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     *->nullable();
      * @return void
      */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('user_id', 20)->unique();
             $table->string('password');
+            $table->string('user_display_name');
+            $table->string('user_phone', 20);
+            $table->string('email');
+            $table->timestamp('type');
             $table->rememberToken();
             $table->timestamps();
         });
